e8f3627bff1e40d1041956849e6007055f63d1ee
Hong <hongzhihua@gmail.com>
2014-05-14 12:32:30 +0800

    Update ae.h

--- src/ae.h
+++ src/ae.h
@@ -89,6 +89,7 @@ typedef struct aeEventLoop {
     int maxfd;   /* highest file descriptor currently registered */
     int setsize; /* max number of file descriptors tracked */
     long long timeEventNextId;
+    /* hong: lastTime is used in processTimeEvents(), to record the time which is called last time */
     time_t lastTime;     /* Used to detect system clock skew */
     aeFileEvent *events; /* Registered events */
     aeFiredEvent *fired; /* Fired events */
